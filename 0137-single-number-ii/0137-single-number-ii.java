0f696e95c9cbbbb48ad819c7fdc2d0b6a63b7e8e
Anshul Sharma <121867290+Propandit@users.noreply.github.com>
2024-07-27 10:58:56 +0530

    Time: 0 ms (100%), Space: 45.5 MB (20.1%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,18 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-          int ans = 0;
-    for(int i = 0; i < 32; i++) {
-        int sum = 0;
-        for(int j = 0; j < nums.length; j++) {
-            if(((nums[j] >> i) & 1) == 1) {
-                sum++;
-                sum %= 3;
-            }
+        int seenOnce =0,seenTwice = 0;
+        for(int num:nums){
+            seenOnce = (seenOnce^num)&(~seenTwice);
+            seenTwice = (seenTwice^num)&(~seenOnce);
         }
-        if(sum != 0) {
-            ans |= sum << i;
-        }
-    }
-    return ans;
+        return seenOnce;
     }
 }
