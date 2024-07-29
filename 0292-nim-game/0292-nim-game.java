3817d0e68ca29349e26069c357150bf65cef7b4a
Anshul Sharma <121867290+Propandit@users.noreply.github.com>
2024-07-29 09:26:44 +0530

    Time: 0 ms (100%), Space: 40.2 MB (41.96%) - LeetHub

---
+++ 0292-nim-game/0292-nim-game.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean canWinNim(int n) {
+        // if(n == 1 || n== 2 || n==3){
+        //     return true;
+        // }
+        // if() 
+      return (n % 4 != 0);
+    }
+}
