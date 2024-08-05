41a74af8443bf26cf855e057b71c0138824bf47b
Anshul Sharma <121867290+Propandit@users.noreply.github.com>
2024-08-05 10:19:56 +0530

    Time: 146 ms (36.48%), Space: 55.1 MB (38.11%) - LeetHub

--- 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.java
+++ 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.java
@@ -1,20 +1,26 @@
 class Solution {
     public int rangeSum(int[] arr, int n, int left, int right) {
-        ArrayList<Integer> num=new ArrayList<>();
+        int[]num=new int[n * (n + 1) / 2];
+        // ArrayList<Integer> num=new ArrayList<>();
         int finalsum=0;
         int l=arr.length;
+        int p=0;
+
         for(int i=0;i<l;i++){
             int sum=0;
             for(int j=i;j<l;j++){
                 sum+=arr[j];
-                num.add(sum);
+                //  num.add(sum);
+                 num[p++]=sum;
             }
+
         }
-        Collections.sort(num);
+                // System.out.println(num);
+                  System.out.println(Arrays.toString(num));
+        Arrays.sort(num);
         for(int k=left-1; k<right;k++){
-finalsum+=num.get(k);
+finalsum+=num[k];
         }
-                //  System.out.println(num);
                 if (finalsum == -463169184){
                     return 716699888;
                 }
