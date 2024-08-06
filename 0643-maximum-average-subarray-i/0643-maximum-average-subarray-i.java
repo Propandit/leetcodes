4ce71f33388f69d120d79dab4aeadbccc23c771d
Anshul Sharma <121867290+Propandit@users.noreply.github.com>
2024-08-06 11:27:33 +0530

    Time: 2 ms (100%), Space: 55.1 MB (95.26%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,13 +1,24 @@
-class Solution {
-    public double findMaxAverage(int[] nums, int k) {
-        int sum = 0;
-       for(int i = 0; i < k; i ++) 
-           sum += nums[i];
-       int maxSum = sum;
-       for(int i = k; i < nums.length; i ++) {
-           sum += nums[i] - nums[i - k];
-           maxSum = Math.max(maxSum, sum);
-       }
-       return (double)maxSum / k;
-       }
+class Solution {  
+    public double findMaxAverage(int[] nums, int k) {  
+        // First, we calculate the sum of the first k elements  
+        int sum = 0;  
+        for (int i = 0; i < k; i++) {  
+            sum += nums[i]; // Add the first k elements to the sum  
+        }  
+
+        // Initialize maxSum to be the sum of the first k elements  
+        int maxSum = sum;  
+
+        // Now we slide the window from the (k)th element to the end of the array  
+        for (int i = k; i < nums.length; i++) {  
+            // Adjust the sum by removing the element that's sliding out and adding the new element  
+            sum += nums[i] - nums[i - k];  
+
+            // Update maxSum if the current sum is greater  
+            maxSum = Math.max(maxSum, sum);  
+        }  
+
+        // Return the maximum average by dividing the max sum by k  
+        return (double)maxSum / k;  
+    }  
 }
