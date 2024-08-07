9227439310ccf987ba59a098807ef9f944796339
Anshul Sharma <121867290+Propandit@users.noreply.github.com>
2024-08-07 10:47:29 +0530

    Time: 2 ms (82.11%), Space: 42.5 MB (25.81%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.java
+++ 0273-integer-to-english-words/0273-integer-to-english-words.java
@@ -1,40 +1,30 @@
 class Solution {
+
+    private final String[] belowTwenty = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
+            "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    private final String[] tens = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+
     public String numberToWords(int num) {
         if (num == 0) {
             return "Zero";
         }
+        return helper(num);
+    }
 
-        String[] ones = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-        String[] tens = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-        String[] thousands = {"", "Thousand", "Million", "Billion"};
-
+    private String helper(int num) {
         StringBuilder result = new StringBuilder();
-        int groupIndex = 0;
-
-        while (num > 0) {
-            if (num % 1000 != 0) {
-                StringBuilder groupResult = new StringBuilder();
-                int part = num % 1000;
-
-                if (part >= 100) {
-                    groupResult.append(ones[part / 100]).append(" Hundred ");
-                    part %= 100;
-                }
-
-                if (part >= 20) {
-                    groupResult.append(tens[part / 10]).append(" ");
-                    part %= 10;
-                }
-
-                if (part > 0) {
-                    groupResult.append(ones[part]).append(" ");
-                }
-
-                groupResult.append(thousands[groupIndex]).append(" ");
-                result.insert(0, groupResult);
-            }
-            num /= 1000;
-            groupIndex++;
+        if (num < 20) {
+            result.append(belowTwenty[num]);
+        } else if (num < 100) {
+            result.append(tens[num / 10]).append(" ").append(belowTwenty[num % 10]);
+        } else if (num < 1000) {
+            result.append(helper(num / 100)).append(" Hundred ").append(helper(num % 100));
+        } else if (num < 1000000) {
+            result.append(helper(num / 1000)).append(" Thousand ").append(helper(num % 1000));
+        } else if (num < 1000000000) {
+            result.append(helper(num / 1000000)).append(" Million ").append(helper(num % 1000000));
+        } else {
+            result.append(helper(num / 1000000000)).append(" Billion ").append(helper(num % 1000000000));
         }
         return result.toString().trim();
     }
