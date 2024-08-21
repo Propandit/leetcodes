3e0421c822c9f93b19ae000000361a6ade023fa8
Anshul Sharma <121867290+Propandit@users.noreply.github.com>
2024-08-21 22:49:29 +0530

    Time: 0 ms (100%), Space: 40.4 MB (71.78%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
@@ -1,28 +1,11 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-         LinkedList<Integer> list = new LinkedList<>();
-        for (int i=0;i<n;i++){
-            list.add(i+1);
-        }
-        int c=0;
-       // int x=1;
-
-        while(list.size()!=1){
-           // System.out.println(list);
-            if(c > list.size() || c==list.size()) c=0;
-           for(int i=0;i<k-1;i++) {
-               if(c > list.size() || c==list.size()) c=0;
-                c++;
-               if(c > list.size() || c==list.size()) c=0;
-           }
-           // if (c > list.size() || c==list.size())  c = 0;
-
-
-               list.remove(c);
-           }
-
-
-
-        return list.getFirst();
+         return helper(n,k)+1;
+}
+public int helper(int n,int k){
+    if(n==1)
+        return 0;
+    else
+        return (helper(n-1,k)+k)%n;
 }
 }
