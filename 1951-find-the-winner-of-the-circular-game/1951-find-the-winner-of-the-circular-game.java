bb1dd5c69266f9a037e453441fb14daa257e2b0b
Anshul Sharma <121867290+Propandit@users.noreply.github.com>
2024-08-21 22:49:14 +0530

    Time: 10 ms (25.42%), Space: 41.3 MB (19.31%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
@@ -1,17 +1,28 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-           ArrayList<Integer> circle = new ArrayList<>();
-        for (int i = 1; i <= n; ++i) {
-            circle.add(i);
+         LinkedList<Integer> list = new LinkedList<>();
+        for (int i=0;i<n;i++){
+            list.add(i+1);
         }
-        int cur_ind = 0;
+        int c=0;
+       // int x=1;
 
-        while (circle.size() > 1) {
-            int next_to_remove = (cur_ind + k - 1) % circle.size();
-            circle.remove(next_to_remove);
-            cur_ind = next_to_remove;
-        }
+        while(list.size()!=1){
+           // System.out.println(list);
+            if(c > list.size() || c==list.size()) c=0;
+           for(int i=0;i<k-1;i++) {
+               if(c > list.size() || c==list.size()) c=0;
+                c++;
+               if(c > list.size() || c==list.size()) c=0;
+           }
+           // if (c > list.size() || c==list.size())  c = 0;
 
-        return circle.get(0);
+
+               list.remove(c);
+           }
+
+
+
+        return list.getFirst();
 }
 }
