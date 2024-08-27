3678d9d0a49b63855c340191f2a87b5f7cc7744b
Anshul Sharma <121867290+Propandit@users.noreply.github.com>
2024-08-27 13:52:56 +0530

    Time: 10 ms (98.38%), Space: 56.6 MB (78.76%) - LeetHub

--- 1325-path-with-maximum-probability/1325-path-with-maximum-probability.java
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.java
@@ -1,17 +1,25 @@
 class Solution {
-      public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) 
-    {
-        double[] ret=new double[n];
-        ret[start]=1;
-        for(int i=0;i<n;++i)
-            for(int j=0;j<edges.length;++j)
-            {
-                int[] e=edges[j];
-                if(ret[e[1]]<ret[e[0]]*succProb[j])
-                    ret[e[1]]=ret[e[0]]*succProb[j]; 
-                else if(ret[e[0]]<ret[e[1]]*succProb[j])
-                    ret[e[0]]=ret[e[1]]*succProb[j];
+    public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
+        double[] probs = new double[n];
+        probs[start_node] = 1;
+        for(int i = 0; i < n - 1; i++) {
+            boolean change = false;
+            for(int j = 0; j < edges.length; j++) {
+                int src = edges[j][0];
+                int dest = edges[j][1];
+                double prob = succProb[j];
+
+                if(probs[src] * prob > probs[dest]) {
+                    probs[dest] = probs[src] * prob;
+                    change = true;
+                }
+                if(probs[dest] * prob > probs[src]) {
+                    probs[src] = probs[dest] * prob;
+                    change = true;
+                }
             }
-        return ret[end];
-    }  
+            if(change == false) break;
+        }
+        return probs[end_node];
+    }
 }
